f3f7a4d471101ab2775e8ec166abfd18242138ad
mahdi <mmagrounmahdi@gmail.com>
2024-10-24 19:06:25 +0100

    init kpi's

--- go.mod
+++ go.mod
@@ -17,15 +17,15 @@ require (
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.9.0
 	github.com/swaggo/echo-swagger v1.1.4
 	github.com/swaggo/swag v1.7.8
 	github.com/valyala/fasttemplate v1.2.1
 	go.uber.org/zap v1.20.0
-	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce
-	golang.org/x/net v0.0.0-20220114011407-0dd24b26b47d // indirect
-	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
-	golang.org/x/tools v0.1.8 // indirect
+	golang.org/x/crypto v0.24.0
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	gopkg.in/boj/redistore.v1 v1.0.0-20160128113310-fc113767cd6b
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0
@@ -42,6 +42,8 @@ require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/spec v0.20.4 // indirect
@@ -60,14 +62,21 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.20.5 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.55.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/time v0.0.0-20211116232009-f0f3c7e86c11 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -9,42 +9,97 @@ import (
 	"github.com/ybkuroki/go-webapp-sample/migration"
 	"github.com/ybkuroki/go-webapp-sample/repository"
 	"github.com/ybkuroki/go-webapp-sample/router"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	// "net/http"
+	"time"
 )
 
 // @title go-webapp-sample API
 // @version 1.5.1
 // @description This is API specification for go-webapp-sample project.
-
 // @license.name MIT
 // @license.url https://opensource.org/licenses/mit-license.php
-
 // @host localhost:8080
 // @BasePath /api
+
+// Prometheus metrics
+var (
+	requestDuration = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name:    "http_request_duration_seconds",
+		Help:    "Duration of HTTP requests in seconds",
+		Buckets: prometheus.DefBuckets,
+	})
+
+	errorCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "http_errors_total",
+		Help: "Total number of HTTP errors",
+	})
+)
+
+// Initialize metrics
+func init() {
+	prometheus.MustRegister(requestDuration)
+	prometheus.MustRegister(errorCounter)
+}
+
+// Middleware to measure latency and count errors
+func metricsMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		start := time.Now()
+
+		// Execute the next handler and capture any errors
+		err := next(c)
+		if err != nil {
+			c.Error(err)
+			errorCounter.Inc() // Increment the error counter if an error occurs
+		}
+
+		// Record the duration of the request
+		duration := time.Since(start).Seconds()
+		requestDuration.Observe(duration)
+
+		return err
+	}
+}
+
 func main() {
 	e := echo.New()
 
+	// Use the metrics middleware
+	e.Use(metricsMiddleware)
+
+	// Prometheus metrics endpoint
+	e.GET("/prometheus", echo.WrapHandler(promhttp.Handler()))
+
+	// Load configuration and initialize logger
 	conf, env := config.Load()
 	logger := logger.NewLogger(env)
-	logger.GetZapLogger().Infof("Loaded this configuration : application." + env + ".yml")
+	logger.GetZapLogger().Infof("Loaded configuration: application.%s.yml", env)
 
+	// Initialize repository and container
 	rep := repository.NewBookRepository(logger, conf)
 	container := container.NewContainer(rep, conf, logger, env)
 
+	// Run database migrations and initialize master data
 	migration.CreateDatabase(container)
 	migration.InitMasterData(container)
 
+	// Initialize routers and middlewares
 	router.Init(e, container)
 	middleware.InitLoggerMiddleware(e, container)
 	middleware.InitSessionMiddleware(e, container)
 
+	// Serve static files if a path is provided
 	if conf.StaticContents.Path != "" {
 		e.Static("/", conf.StaticContents.Path)
-		logger.GetZapLogger().Infof("Served the static contents. path: " + conf.StaticContents.Path)
+		logger.GetZapLogger().Infof("Served static contents. Path: %s", conf.StaticContents.Path)
 	}
 
+	// Start the server
 	if err := e.Start(":8000"); err != nil {
-		logger.GetZapLogger().Errorf(err.Error())
+		logger.GetZapLogger().Errorf("Error starting server: %s", err.Error())
 	}
 
-	defer rep.Close()
+	defer rep.Close() // Ensure the repository is closed on exit
 }
